f38d5e942ccb6eca9981c09a78e5661eb6994358
Pavel Loginov <aidaho@roxy-wi.org>
2022-08-12 09:40:35 +0300

    v0.1

--- README.md
+++ README.md
@@ -1,2 +1,128 @@
-# roxy-wi-prometheus-exporter
-Roxy-WI Prometheus exporter
+# Roxy-WI Prometheus exporter
+
+Roxy-WI Prometheus exporter is on beta now. If you have a lack of metrics or faced 
+with any problems, please [write to us](https://roxy-wi.org/contacts).
+
+Roxy-WI Prometheus exporter exposes next metrics:
+
+```# HELP python_gc_objects_collected_total Objects collected during gc
+# TYPE python_gc_objects_collected_total counter
+python_gc_objects_collected_total{generation="0"} 1180.0
+python_gc_objects_collected_total{generation="1"} 265.0
+python_gc_objects_collected_total{generation="2"} 0.0
+# HELP python_gc_objects_uncollectable_total Uncollectable object found during GC
+# TYPE python_gc_objects_uncollectable_total counter
+python_gc_objects_uncollectable_total{generation="0"} 0.0
+python_gc_objects_uncollectable_total{generation="1"} 0.0
+python_gc_objects_uncollectable_total{generation="2"} 0.0
+# HELP python_gc_collections_total Number of times this generation was collected
+# TYPE python_gc_collections_total counter
+python_gc_collections_total{generation="0"} 82.0
+python_gc_collections_total{generation="1"} 7.0
+python_gc_collections_total{generation="2"} 0.0
+# HELP python_info Python platform information
+# TYPE python_info gauge
+python_info{implementation="CPython",major="3",minor="6",patchlevel="8",version="3.6.8"} 1.0
+# HELP process_virtual_memory_bytes Virtual memory size in bytes.
+# TYPE process_virtual_memory_bytes gauge
+process_virtual_memory_bytes 3.64716032e+08
+# HELP process_resident_memory_bytes Resident memory size in bytes.
+# TYPE process_resident_memory_bytes gauge
+process_resident_memory_bytes 2.4092672e+07
+# HELP process_start_time_seconds Start time of the process since unix epoch in seconds.
+# TYPE process_start_time_seconds gauge
+process_start_time_seconds 1.66028387313e+09
+# HELP process_cpu_seconds_total Total user and system CPU time spent in seconds.
+# TYPE process_cpu_seconds_total counter
+process_cpu_seconds_total 1.74
+# HELP process_open_fds Number of open file descriptors.
+# TYPE process_open_fds gauge
+process_open_fds 7.0
+# HELP process_max_fds Maximum number of open file descriptors.
+# TYPE process_max_fds gauge
+process_max_fds 1024.0
+# HELP roxy_wi_services_total How many services
+# TYPE roxy_wi_services_total gauge
+roxy_wi_services_total{name="HAProxy",service="haproxy"} 2.0
+roxy_wi_services_total{name="NGINX",service="nginx"} 3.0
+roxy_wi_services_total{name="Keepalived",service="keepalived"} 3.0
+roxy_wi_services_total{name="Apache",service="apache"} 1.0
+# HELP roxy_wi_server_total How many servers
+# TYPE roxy_wi_server_total gauge
+roxy_wi_server_total 3.0
+# HELP roxy_wi_enabled_server_total How many enabled servers
+# TYPE roxy_wi_enabled_server_total gauge
+roxy_wi_enabled_server_total 3.0
+# HELP roxy_wi_virtual_server_total How many virtual servers
+# TYPE roxy_wi_virtual_server_total gauge
+roxy_wi_virtual_server_total 0.0
+# HELP roxy_wi_user_total How many users
+# TYPE roxy_wi_user_total gauge
+roxy_wi_user_total 3.0
+# HELP roxy_wi_user_by_role Users by roles
+# TYPE roxy_wi_user_by_role gauge
+roxy_wi_user_by_role{role="superAdmin"} 1.0
+roxy_wi_user_by_role{role="admin"} 1.0
+roxy_wi_user_by_role{role="user"} 1.0
+roxy_wi_user_by_role{role="guest"} 0.0
+# HELP roxy_wi_service_checker_enabled_total How many Checkers are enabled for service
+# TYPE roxy_wi_service_checker_enabled_total gauge
+roxy_wi_service_checker_enabled_total{name="HAProxy",service="haproxy"} 2.0
+roxy_wi_service_checker_enabled_total{name="NGINX",service="nginx"} 3.0
+roxy_wi_service_checker_enabled_total{name="Keepalived",service="keepalived"} 3.0
+roxy_wi_service_checker_enabled_total{name="Apache",service="apache"} 1.0
+# HELP roxy_wi_checker_enabled_total How many Checkers are enabled
+# TYPE roxy_wi_checker_enabled_total gauge
+roxy_wi_checker_enabled_total 9.0
+# HELP roxy_wi_service_checker_alert Number of alerts per server
+# TYPE roxy_wi_service_checker_alert gauge
+roxy_wi_service_checker_alert{hostname="test-server",ip="10.0.0.205",level="info"} 10.0
+roxy_wi_service_checker_alert{hostname="test-server",ip="10.0.0.205",level="warning"} 2.0
+roxy_wi_service_checker_alert{hostname="test-server-2",ip="127.0.0.2",level="info"} 321.0
+roxy_wi_service_checker_alert{hostname="test-server-2",ip="127.0.0.2",level="warning"} 64.0
+roxy_wi_service_checker_alert{hostname="test-server-3",ip="127.0.0.4",level="info"} 266.0
+roxy_wi_service_checker_alert{hostname="test-server-3",ip="127.0.0.4",level="warning"} 35.0
+# HELP roxy_wi_service_checker_alert_total Total alerts
+# TYPE roxy_wi_service_checker_alert_total gauge
+roxy_wi_service_checker_alert_total{level="info"} 597.0
+roxy_wi_service_checker_alert_total{level="warning"} 101.0
+# HELP roxy_wi_service_auto_start_enabled_total How many Auto start are enabled for service
+# TYPE roxy_wi_service_auto_start_enabled_total gauge
+roxy_wi_service_auto_start_enabled_total{name="HAProxy",service="haproxy"} 2.0
+roxy_wi_service_auto_start_enabled_total{name="NGINX",service="nginx"} 3.0
+roxy_wi_service_auto_start_enabled_total{name="Keepalived",service="keepalived"} 3.0
+roxy_wi_service_auto_start_enabled_total{name="Apache",service="apache"} 1.0
+# HELP roxy_wi_auto_start_enabled_total How many Auto start are enabled
+# TYPE roxy_wi_auto_start_enabled_total gauge
+roxy_wi_auto_start_enabled_total 9.0
+# HELP roxy_wi_service_metrics_enabled_total How many Metrics are enabled for service
+# TYPE roxy_wi_service_metrics_enabled_total gauge
+roxy_wi_service_metrics_enabled_total{name="HAProxy",service="haproxy"} 2.0
+roxy_wi_service_metrics_enabled_total{name="NGINX",service="nginx"} 2.0
+roxy_wi_service_metrics_enabled_total{name="Apache",service="apache"} 1.0
+# HELP roxy_wi_metrics_enabled_total How many Metrics are enabled
+# TYPE roxy_wi_metrics_enabled_total gauge
+roxy_wi_metrics_enabled_total 5.0
+```
+
+# Installation
+
+To install the exporter:
+
+```angular2html
+git clone https://github.com/hap-wi/roxy-wi-prometheus-exporter.git
+cd roxy-wi-prometheus-exporter
+pip3 install -r requirements.txt
+chmod +x prometheus_exporter.py
+mv prometheus_exporter.py /var/www/haproxy-wi/app/tools/
+```
+
+# Run:
+```angular2html
+cd /var/www/haproxy-wi/app
+tools/prometheus_exporter.py &
+```
+
+# Dashboard
+There is [dashboard for Grafana](https://grafana.com/grafana/dashboards/16713)
+
---
+++ promethus_exporter.py
@@ -0,0 +1,316 @@
+#!/usr/bin/env python3
+import os
+import sys
+from functools import lru_cache
+
+import argparse
+from prometheus_client.core import GaugeMetricFamily, REGISTRY
+from prometheus_client import make_wsgi_app
+from wsgiref.simple_server import make_server
+
+metrics_app = make_wsgi_app()
+
+sys.path.append(os.path.join(sys.path[0], os.path.dirname(os.getcwd())))
+sys.path.append(os.path.join(sys.path[0], os.getcwd()))
+import sql
+
+
+@lru_cache(1)
+def get_services_from_db():
+    services = sql.select_services()
+
+    return services
+
+
+@lru_cache(1)
+def get_servers_from_db():
+    servers = sql.select_servers(full=1)
+
+    return servers
+
+
+@lru_cache(1)
+def get_users_from_db():
+    users = sql.select_users()
+
+    return users
+
+
+def get_service_name() -> dict:
+    """Make a dict with services Name and Slug"""
+    services_name = {}
+    services = get_services_from_db()
+
+    for service in services:
+        services_name[service.slug] = service.service
+
+    return services_name
+
+
+class GeneralInfo(object):
+    """Collect general info about Roxy-WI"""
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def collect():
+        virtual = 0
+        enabled = 0
+
+        servers = get_servers_from_db()
+        users = get_users_from_db()
+        services = get_services_from_db()
+        service_total = GaugeMetricFamily("roxy_wi_services_total", "How many services", labels=['service', 'name'])
+        enabled_server_total = GaugeMetricFamily("roxy_wi_enabled_server_total", "How many enabled servers")
+        virtual_server_total = GaugeMetricFamily("roxy_wi_virtual_server_total", "How many virtual servers")
+        user_total = GaugeMetricFamily("roxy_wi_user_total", "How many users")
+        user_by_role = GaugeMetricFamily("roxy_wi_user_by_role", "Users by roles", labels=['role'])
+        server_total = GaugeMetricFamily("roxy_wi_server_total", "How many servers")
+
+        for server in servers:
+            if server[4] == 1:
+                virtual += 1
+            if server[5] == 1:
+                enabled += 1
+
+        for service in services:
+            service_count = sql.select_count_services(service.slug)
+            service_total.add_metric([service.slug, service.service], service_count)
+
+        user_superadmin = 0
+        user_admin = 0
+        user_user = 0
+        user_guest = 0
+
+        for user in users:
+            if user.role == 'superAdmin':
+                user_superadmin += 1
+            if user.role == 'admin':
+                user_admin += 1
+            if user.role == 'user':
+                user_user += 1
+            if user.role == 'guest':
+                user_guest += 1
+
+        server_total.add_metric([''], len(servers))
+        enabled_server_total.add_metric([''], enabled)
+        virtual_server_total.add_metric([''], virtual)
+        user_total.add_metric([''], len(users))
+        user_by_role.add_metric(['superAdmin'], user_superadmin)
+        user_by_role.add_metric(['admin'], user_admin)
+        user_by_role.add_metric(['user'], user_user)
+        user_by_role.add_metric(['guest'], user_guest)
+        yield service_total
+        yield server_total
+        yield enabled_server_total
+        yield virtual_server_total
+        yield user_total
+        yield user_by_role
+
+
+class ServiceChecker(object):
+    """Collect metrics about the Checker service"""
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def collect():
+        checker_haproxy = 0
+        checker_nginx = 0
+        checker_keepalived = 0
+        checker_apache = 0
+
+        service_checker_enabled_total = GaugeMetricFamily("roxy_wi_service_checker_enabled_total",
+                                                          "How many Checkers are enabled for service",
+                                                          labels=['service', 'name'])
+        checker_enabled_total = GaugeMetricFamily("roxy_wi_checker_enabled_total", "How many Checkers are enabled")
+
+        # Fetch raw status data from the cache
+        servers = get_servers_from_db()
+        services_name = get_service_name()
+
+        # Update Prometheus metrics
+        for server in servers:
+            if server[8] == 1:
+                checker_haproxy += 1
+            if server[19] == 1:
+                checker_nginx += 1
+            if server[23] == 1:
+                checker_keepalived += 1
+            if server[25] == 1:
+                checker_apache += 1
+
+        checker_total = checker_haproxy + checker_nginx + checker_keepalived + checker_apache
+
+        service_checker_enabled_total.add_metric(['haproxy', services_name['haproxy']], checker_haproxy)
+        service_checker_enabled_total.add_metric(['nginx', services_name['nginx']], checker_nginx)
+        service_checker_enabled_total.add_metric(['keepalived', services_name['keepalived']], checker_keepalived)
+        service_checker_enabled_total.add_metric(['apache', services_name['apache']], checker_apache)
+        checker_enabled_total.add_metric([''], checker_total)
+
+        yield service_checker_enabled_total
+        yield checker_enabled_total
+
+        # Fetch alerts
+        info_alerts = 0
+        warning_alerts = 0
+        alerts = sql.alerts_history('Checker', 1)
+        alert_by_server = {}
+
+        service_checker_alert = GaugeMetricFamily(
+            "roxy_wi_service_checker_alert",
+            "Number of alerts per server",
+            labels=['hostname', 'ip', 'level']
+        )
+        service_checker_alert_total = GaugeMetricFamily(
+            "roxy_wi_service_checker_alert_total",
+            "Total alerts",
+            labels=['level']
+        )
+
+        for server in servers:
+            alert_by_server.setdefault(server[2])
+            alert_by_server[server[2]] = {'hostname': server[1], 'info': 0, 'warning': 0}
+
+        for alert in alerts:
+            if alert[1] == 'info':
+                info_alerts += 1
+                alert_by_server[alert[2]]['info'] += 1
+            else:
+                warning_alerts += 1
+                alert_by_server[alert[2]]['warning'] += 1
+
+        for server_ip, value in alert_by_server.items():
+            service_checker_alert.add_metric([value['hostname'], server_ip, 'info'], value['info'])
+            service_checker_alert.add_metric([value['hostname'], server_ip, 'warning'], value['warning'])
+
+        service_checker_alert_total.add_metric(['info'], info_alerts)
+        service_checker_alert_total.add_metric(['warning'], warning_alerts)
+
+        yield service_checker_alert
+        yield service_checker_alert_total
+
+
+class AutoStartChecker(object):
+    """Collect metrics about the Checker service"""
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def collect():
+        auto_start_haproxy = 0
+        auto_start_nginx = 0
+        auto_start_keepalived = 0
+        auto_start_apache = 0
+
+        service_auto_start_enabled_total = GaugeMetricFamily("roxy_wi_service_auto_start_enabled_total",
+                                                             "How many Auto start are enabled for service",
+                                                             labels=['service', 'name'])
+        auto_start_enabled_total = GaugeMetricFamily("roxy_wi_auto_start_enabled_total",
+                                                     "How many Auto start are enabled")
+
+        # Fetch raw status data from the cache
+        servers = get_servers_from_db()
+        services_name = get_service_name()
+
+        # Update Prometheus metrics
+        for server in servers:
+            if server[12] == 1:
+                auto_start_haproxy += 1
+            if server[17] == 1:
+                auto_start_nginx += 1
+            if server[22] == 1:
+                auto_start_keepalived += 1
+            if server[25] == 1:
+                auto_start_apache += 1
+
+        auto_start_total = auto_start_haproxy + auto_start_nginx + auto_start_keepalived + auto_start_apache
+        service_auto_start_enabled_total.add_metric(['haproxy', services_name['haproxy']], auto_start_haproxy)
+        service_auto_start_enabled_total.add_metric(['nginx', services_name['nginx']], auto_start_nginx)
+        service_auto_start_enabled_total.add_metric(['keepalived', services_name['keepalived']], auto_start_keepalived)
+        service_auto_start_enabled_total.add_metric(['apache', services_name['apache']], auto_start_apache)
+        auto_start_enabled_total.add_metric([''], auto_start_total)
+
+        yield service_auto_start_enabled_total
+        yield auto_start_enabled_total
+
+
+class MetricsChecker(object):
+    """Collect metrics about the Metrics service"""
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def collect():
+        metrics_haproxy = 0
+        metrics_nginx = 0
+        metrics_apache = 0
+
+        servers = get_servers_from_db()
+        services_name = get_service_name()
+        service_metrics_enabled_total = GaugeMetricFamily("roxy_wi_service_metrics_enabled_total",
+                                                          "How many Metrics are enabled for service",
+                                                          labels=['service', 'name'])
+        metrics_enabled_total = GaugeMetricFamily("roxy_wi_metrics_enabled_total", "How many Metrics are enabled")
+
+        for server in servers:
+            if server[9] == 1:
+                metrics_haproxy += 1
+            if server[12] == 1:
+                metrics_nginx += 1
+            if server[27] == 1:
+                metrics_apache += 1
+
+        metrics_total = metrics_haproxy + metrics_nginx + metrics_apache
+
+        service_metrics_enabled_total.add_metric(['haproxy', services_name['haproxy']], metrics_haproxy)
+        service_metrics_enabled_total.add_metric(['nginx', services_name['nginx']], metrics_nginx)
+        service_metrics_enabled_total.add_metric(['apache', services_name['apache']], metrics_apache)
+        metrics_enabled_total.add_metric([''], metrics_total)
+
+        yield service_metrics_enabled_total
+        yield metrics_enabled_total
+
+
+def expose_metrics(environ, start_fn):
+    if environ['PATH_INFO'] == '/metrics':
+        return metrics_app(environ, start_fn)
+    start_fn('200 OK', [])
+    return [b'<a href="/metrics">/metrics</a>']
+
+
+def main():
+    """Main entry point"""
+
+    parser = argparse.ArgumentParser(description='Roxy-WI Prometheus exporter.', prog='prometheus_exporter.py',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument(
+        '--address',
+        help='Address on which to expose metrics and web interface.',
+        nargs='?',
+        type=str,
+        default='0.0.0.0:9900'
+    )
+
+    args = parser.parse_args()
+    try:
+        bind_ip = args.address.split(':')[0]
+        bind_port = int(args.address.split(':')[1])
+    except Exception:
+        print('error: wrong IP:PORT format')
+
+    REGISTRY.register(GeneralInfo())
+    REGISTRY.register(ServiceChecker())
+    REGISTRY.register(AutoStartChecker())
+    REGISTRY.register(MetricsChecker())
+    httpd = make_server(bind_ip, bind_port, expose_metrics)
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    main()
